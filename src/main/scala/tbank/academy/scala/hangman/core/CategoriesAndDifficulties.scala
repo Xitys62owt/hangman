b3eb2ec39d4176b2e9d09f9aec1840937fecbfe5
Xitys62owt <martynenko.arsenii@phystech.edu>
2025-09-29 21:12:24 +0300

    Fix scalafmt to v3

--- src/main/scala/tbank/academy/scala/hangman/core/CategoriesAndDifficulties.scala
+++ src/main/scala/tbank/academy/scala/hangman/core/CategoriesAndDifficulties.scala
@@ -17,32 +17,32 @@ enum CategoryName:
 
 object Difficulty:
   def toStringRus(difficulty: Difficulty): String = difficulty match
-    case Noob => "Новичок"
-    case Easy => "Легкий"
-    case Medium => "Средний"
-    case Hard => "Сложный"
-    case Pain => "Болезненный"
+    case Noob       => "Новичок"
+    case Easy       => "Легкий"
+    case Medium     => "Средний"
+    case Hard       => "Сложный"
+    case Pain       => "Болезненный"
     case Impossible => "Невозможный"
 
   def toStringEng(difficulty: Difficulty): String = difficulty match
-    case Noob => "Noob"
-    case Easy => "Easy"
-    case Medium => "Medium"
-    case Hard => "Hard"
-    case Pain => "Pain"
+    case Noob       => "Noob"
+    case Easy       => "Easy"
+    case Medium     => "Medium"
+    case Hard       => "Hard"
+    case Pain       => "Pain"
     case Impossible => "Impossible"
 
 object CategoryName:
   def toStringRus(category: CategoryName): String = category match
-    case Animals => "Животные"
-    case Plants => "Растения"
-    case CountriesAndCities => "Страны и города"
-    case GeographicalNames => "Географические названия"
+    case Animals              => "Животные"
+    case Plants               => "Растения"
+    case CountriesAndCities   => "Страны и города"
+    case GeographicalNames    => "Географические названия"
     case ScienceAndTechnology => "Наука и технологии"
 
   def toStringEng(category: CategoryName): String = category match
-    case Animals => "Animals"
-    case Plants => "Plants"
-    case CountriesAndCities => "CountriesAndCities"
-    case GeographicalNames => "GeographicalNames"
+    case Animals              => "Animals"
+    case Plants               => "Plants"
+    case CountriesAndCities   => "CountriesAndCities"
+    case GeographicalNames    => "GeographicalNames"
     case ScienceAndTechnology => "ScienceAndTechnology"
--- src/main/scala/tbank/academy/scala/hangman/error/DomainError.scala
+++ src/main/scala/tbank/academy/scala/hangman/error/DomainError.scala
@@ -1,9 +1,7 @@
 package tbank.academy.scala.hangman.error
 
-sealed trait DomainError
-object DomainError {
-  case object InvalidLetter           extends DomainError
-  case object NoWordsAvailable        extends DomainError
-  case object NoCategoriesAvailable   extends DomainError
-  case object NoDifficultiesAvailable extends DomainError
-}
+enum DomainError:
+  case InvalidLetter
+  case NoWordsAvailable
+  case NoCategoriesAvailable
+  case NoDifficultiesAvailable
